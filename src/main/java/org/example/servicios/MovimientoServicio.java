a730cd1a188d74aee3a7a2d123b013005e929c3b
CroquetaConQueso <lacio161@gmail.com>
2026-03-24 09:00:24 +0100

    feat: saltos de linea, devolver listas, output feedback

--- src/main/java/org/example/servicios/MovimientoServicio.java
+++ src/main/java/org/example/servicios/MovimientoServicio.java
@@ -7,8 +7,11 @@ import org.example.modelos.TipoMovimiento;
 import org.example.repositorio.RepositorioCuenta;
 import org.example.repositorio.RepositorioMovimiento;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @AllArgsConstructor
 public class MovimientoServicio {
@@ -23,6 +26,9 @@ public class MovimientoServicio {
     }
 
     public Cuenta depositar(String numeroCuenta, BigDecimal cantidad){
+        if(cantidad ==null || cantidad.compareTo(BigDecimal.ZERO)<=0){
+            throw new IllegalArgumentException("La cantidad a depositar debe ser mayor que cero");
+        }
         Cuenta cuenta = repoCuenta.buscarNumeroCuenta(numeroCuenta);
 
         if(cuenta == null){
@@ -36,10 +42,18 @@ public class MovimientoServicio {
     }
 
     public Cuenta retirar(String numeroCuenta, BigDecimal cantidad){
+        if(cantidad ==null || cantidad.compareTo(BigDecimal.ZERO)<=0){
+            throw new IllegalArgumentException("La cantidad a retirar debe ser mayor que cero");
+        }
         Cuenta cuenta = repoCuenta.buscarNumeroCuenta(numeroCuenta);
 
         if(cuenta == null){
             throw new IllegalArgumentException("No se ha encontrado la cuenta");
+        }
+
+        if (cuenta.getSaldoCuenta().compareTo(cantidad) < 0) {
+            throw new IllegalArgumentException("Saldo insuficiente.\nSaldo disponible: " + cuenta.getSaldoCuenta()
+                    + " €\nImporte solicitado: " + cantidad + " €");
         }
 
         cuenta.setSaldoCuenta(cuenta.getSaldoCuenta().subtract(cantidad));
@@ -49,8 +63,16 @@ public class MovimientoServicio {
     }
 
     public void transferir(String numeroOrigen, String numeroDestino, BigDecimal cantidad){
+        if(cantidad ==null || cantidad.compareTo(BigDecimal.ZERO)<=0){
+            throw new IllegalArgumentException("La cantidad a depositar debe ser mayor que cero");
+        }else if(numeroOrigen.equals(numeroDestino)){
+            throw new IllegalArgumentException("La cuenta origen y destino deben de ser diferentes");
+        }
 
         Cuenta cuentaOrigen = repoCuenta.buscarNumeroCuenta(numeroOrigen);
+        if(cuentaOrigen == null){
+            throw new IllegalArgumentException("La cuenta a realizar la transferencia debe de existir");
+        }
         Cuenta cuentaDestino = repoCuenta.buscarNumeroCuenta(numeroDestino);
 
         if(cuentaDestino == null){
@@ -60,8 +82,39 @@ public class MovimientoServicio {
         if((cuentaOrigen.getSaldoCuenta().compareTo(cantidad))<0){
             throw new IllegalArgumentException("La cantidad a transferir no puede ser mayor que la cantidad encontrada en la cuenta");
         }
+        try {
+            cuentaOrigen.setSaldoCuenta(cuentaOrigen.getSaldoCuenta().subtract(cantidad));
+            cuentaDestino.setSaldoCuenta(cuentaDestino.getSaldoCuenta().add(cantidad));
+            registrarMovimiento(cuentaOrigen, TipoMovimiento.TRANSFERENCIA_SALIENTE, cantidad);
+            registrarMovimiento(cuentaDestino, TipoMovimiento.TRANSFERENCIA_ENTRANTE, cantidad);
+        }catch(Exception ex){
+            cuentaOrigen.setSaldoCuenta(cuentaOrigen.getSaldoCuenta().add(cantidad));
+            cuentaDestino.setSaldoCuenta(cuentaDestino.getSaldoCuenta().subtract(cantidad));
+            throw new RuntimeException("Error durante la transferencia. Se han revertido los datos en memoria.");
+        }
+    }
 
-        cuentaOrigen.setSaldoCuenta(cuentaOrigen.getSaldoCuenta().subtract(cantidad));
-        cuentaDestino.setSaldoCuenta(cuentaDestino.getSaldoCuenta().add(cantidad));
+    public List<Movimiento> obtenerLista(String numeroCuenta){
+        Cuenta cuenta = repoCuenta.buscarNumeroCuenta(numeroCuenta);
+
+        if(cuenta==null){
+            throw new IllegalArgumentException("No se ha encontrado la cuenta");
+        }
+        return repoMovi.obtenerMovimientosCuenta(cuenta.getNumeroCuenta());
+    }
+
+    public List<Movimiento> obtenerListaFecha(String numeroCuenta, LocalDate fechaIn, LocalDate fechaFin){
+        Cuenta cuenta = repoCuenta.buscarNumeroCuenta(numeroCuenta);
+
+        if(cuenta==null){
+            throw new IllegalArgumentException("No se ha encontrado la cuenta");
+        }
+
+        if(fechaIn.isAfter(fechaFin)){
+            throw new IllegalArgumentException("La fecha de inicio no puede ser más antigua que la fecha final");
+        }else if(fechaFin.isBefore(fechaIn)){
+            throw new IllegalArgumentException("La fecha de fin no puede ser anterior a la fecha inicial");
+        }
+        return repoMovi.obtenerMovimientosFecha(numeroCuenta,fechaIn,fechaFin);
     }
 }
